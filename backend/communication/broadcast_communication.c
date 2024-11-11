a95d2b87b561b64cc2956120056134354d001005
Vítor Hugo Pereira Couceiro <vitopereira486@gmail.com>
2024-11-11 23:37:10 +0000

    dia 11

--- backend/communication/broadcast_communication.c
+++ backend/communication/broadcast_communication.c
@@ -3,19 +3,6 @@
 #include "../models/comunicacao.h"
 #include "../communication/unicast_communication.h"
 
-/*
-void broadcastMsg(const TDATA *td, Comunicacao comunicacao) {
-    char feed_pipe [256];
-    strcpy(feed_pipe, comunicacao.user.FEED_PIPE);
-    for (int i = 0; i < td->n_users; i++) {
-        if(strcmp(feed_pipe, td->user[i].nome) != 0){
-            strcpy(comunicacao.user.FEED_PIPE, td->user[i].FEED_PIPE);
-            unicastMsg(comunicacao);
-        }
-    }
-}
-*/
-
 void broadcastUserExpelled(const TDATA *td,ResponseInfoError responseInfoError) {
     for (int i = 0; i < td->n_users; i++) {
         printf("%s\n", td->user[i].FEED_PIPE);

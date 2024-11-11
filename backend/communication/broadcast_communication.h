3db3972e027fea8a7e22f4e2c59cdbb441895950
Vítor Hugo Pereira Couceiro <vitopereira486@gmail.com>
2024-11-11 23:57:56 +0000

    dia 11

--- backend/communication/broadcast_communication.h
+++ backend/communication/broadcast_communication.h
@@ -2,7 +2,6 @@
 #define BROADCAST_COMMUNICATION_H
 #include "../models/comunicacao.h"
 #include "../manager.h"
-//void broadcastMsg(TDATA *td, Comunicacao comunicacao);
 void broadcastUserExpelled(const TDATA *td,ResponseInfoError responseInfoError);
 void broadcastMsg(const TDATA *td, ResponseMsg responseMsg,int topicIndex);
 #endif //BROADCAST_COMMUNICATION_H
